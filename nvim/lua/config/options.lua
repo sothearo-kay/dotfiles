b249d6b8cd3e9406127e3f1a3ca136d333774f54
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-07 16:06:57 +0700

    lazyVim starter

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
 vim.g.mapleader = " "
 
 local opt = vim.opt
@@ -50,5 +54,5 @@ vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
 vim.cmd("let g:netrw_liststyle = 3")
 
 if vim.fn.has("nvim-0.8") == 1 then
-	opt.cmdheight = 0
+  opt.cmdheight = 0
 end
