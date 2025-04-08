2261260d3d355b82a8a055a2c01160d190802c7a
sothearo-kay <sothearo.kay@techostartup.center>
2025-04-08 16:56:00 +0700

    feat: add debugger

--- nvim/lua/plugins/ui/incline.lua
+++ nvim/lua/plugins/ui/incline.lua
@@ -3,19 +3,27 @@ return {
   event = "BufReadPre",
   lazy = true,
   config = function()
+    local helpers = require("incline.helpers")
+    local devicons = require("nvim-web-devicons")
     require("incline").setup({
-      window = { margin = { vertical = 0, horizontal = 1 } },
-      hide = {
-        cursorline = true,
+      window = {
+        padding = 0,
+        margin = { horizontal = 0 },
       },
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        if vim.bo[props.buf].modified then
-          filename = "[+] " .. filename
+        if filename == "" then
+          filename = "[No Name]"
         end
-
-        local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-        return { { icon, guifg = color }, { " " }, { filename } }
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+        return {
+          ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+          " ",
+          { filename, gui = modified and "bold,italic" or "bold" },
+          " ",
+          guibg = "#44406e",
+        }
       end,
     })
   end,
